d5870c7e5797714a52f47671828eeb88c07732f9
Chengze Shen <zhazhashen@gmail.com>
2020-09-14 12:31:37 -0400

    rerunning NJ with FastME

--- NJ/pipeline_nj.sh
+++ NJ/pipeline_nj.sh
@@ -4,34 +4,32 @@
 rep=19
 
 # treebest NJ binary directory
-bin=$(pwd)/../../treebest
+#bin=$(pwd)/../../treebest
 
 # run for 1000M1
-target=1000M1
-if [ ! -d $target ]; then
-        mkdir $target
-fi
-prefix1=$(pwd)/../../$target/$target/R
-for i in $(seq 0 $rep); do
-        echo -en "\rRunning for $target rep $i"
-        if [ ! -d $target/R$i ]; then
-                mkdir $target/R$i
-        fi
-        $bin/treebest nj $prefix1$i/rose.aln.true.phylip > $target/R$i/rose.aln.true.nhx
-done
-echo -e ""
+declare -a targets=(1000M1 1000M4)
+declare -a methods=(p-distance LogDet JC69)
 
-# run for 1000M4
-target=1000M4
-if [ ! -d $target ]; then
-        mkdir $target
-fi
-prefix2=$(pwd)/../../$target/$target/R
-for i in $(seq 0 $rep); do
-        echo -en "\rRunning for $target rep $i"
-        if [ ! -d $target/R$i ]; then
-                mkdir $target/R$i
+for t in ${targets[@]}; do
+        prefix=$(pwd)/../../$t/$t/R
+        if [ ! -d $t ]; then
+                mkdir $t
         fi
-        $bin/treebest nj $prefix2$i/rose.aln.true.phylip > $target/R$i/rose.aln.true.nhx
+
+        for m in ${methods[@]}; do
+                outdir=$t/$m        
+                if [ ! -d $t/$m ]; then
+                        mkdir $t/$m
+                fi
+                for i in $(seq 0 $rep); do
+                        echo -en "\rRunning for $t rep $i, method $m"
+                        out=$outdir/R$i
+                        if [ ! -d $out ]; then
+                                mkdir $out
+                        fi
+                        #$bin/treebest nj $prefix1$i/rose.aln.true.phylip > $target/R$i/rose.aln.true.nhx
+                        fastme -i $prefix$i/rose.aln.true.phylip -o $out/out_tree.nwk -d $m -m N -b 100 -O $out/out_matrix.txt
+                done
+                echo -e ""
+        done
 done
-echo -e ""
