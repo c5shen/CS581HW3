791f79ae24b506eb8b24cdaf79af62c4800abfbb
Chengze Shen <zhazhashen@gmail.com>
2020-09-13 21:20:42 -0400

    finished NJ pipeline

--- NJ/pipeline_nj.sh
+++ NJ/pipeline_nj.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 
+# number of repetitions
+rep=19
+
+# treebest NJ binary directory
+bin=$(pwd)/../../treebest
+
+# run for 1000M1
+target=1000M1
+if [ ! -d $target ]; then
+        mkdir $target
+fi
+prefix1=$(pwd)/../../$target/$target/R
+for i in $(seq 0 $rep); do
+        if [ ! -d $target/R$i ]; then
+                mkdir $target/R$i
+        fi
+        $bin/treebest nj $prefix1$i/rose.aln.true.phylip > $target/R$i/rose.aln.true.nhx
+done
+
+# run for 1000M4
+target=1000M4
+if [ ! -d $target ]; then
+        mkdir $target
+fi
+prefix2=$(pwd)/../../$target/$target/R
+for i in $(seq 0 $rep); do
+        if [ ! -d $target/R$i ]; then
+                mkdir $target/R$i
+        fi
+        $bin/treebest nj $prefix2$i/rose.aln.true.phylip > $target/R$i/rose.aln.true.nhx
+done
 
